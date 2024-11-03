bf6e782c557dfbe4028edf62228f81abcfb133ba
shivam45783 <shivamrajan01@gmail.com>
2024-11-03 13:06:23 +0530

    About_us.py

--- About_us.py
+++ About_us.py
@@ -4,10 +4,10 @@ def run():
     st.write("""
 # Team 1 Project (In Decreasing Order)
 ### Team Members
-1. Ankit Kumar (2403104): Code for displaying board and graph and logic part
-2. Shivam Rajan (2404233): Code for logic part and styling
-3. Tanuja Potnuru (2404229) and Ajinkya Temburne (2404238): Finding images
-4. Dasarapalli Paul Nikesh (2403114): Attended all meetings seriously 
-5. Avani Jain (2406309):  
+1. Ankit Kumar (2403104): Code for displaying board and graph,Logic part and Debugging
+2. Shivam Rajan (2404233): Code for Logic part,Styling and Debugging
+3. Tanuja Potnuru (2404229) and Ajinkya Temburne (2404238): Attended all meetings seriously and Debugging
+4. Dasarapalli Paul Nikesh (2403114): Attended all meetings seriously and Debugging
+5. Avani Jain (2406309):  Debugging
 """)
     

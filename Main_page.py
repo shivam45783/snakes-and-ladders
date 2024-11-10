b082d2cb8af0abdd3a43c390250b4ca06bac8fcd
shivam45783 <shivamrajan01@gmail.com>
2024-11-10 14:01:00 +0530

    update changes

--- Main_page.py
+++ Main_page.py
@@ -1,16 +1,3 @@
-"""
-    Participation in increasing order:
-    1.)Avani Jain (2406309): Attended all meetings seriously,Debugging and created About_us.py
-    2.)Dasarapalli Paul Nikesh (2403114): Attended all meetings seriously,Debugging,Editting and created rules.py
-    3.)Tanuja Potnuru (2404229): Attended all meetings seriously and Debugging and created resourses_refered.py
-    4.)Ajinkya Temburne (2404238): Code for Player Vs Computer Mode
-    5.)Shivam Rajan (2404233): Code for Logic part,Styling and Debugging
-    6.)Ankit Kumar (2403104): Code for displaying board and graph,Logic part and Debugging and Player Vs Player
-    Members: 1,2,3,4 have equale contribution in project
-"""
-
-
-
 import streamlit as st
 from streamlit_option_menu import option_menu
 import Home
